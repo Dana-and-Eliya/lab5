121491f7e001ce0645851a6680c546fe2c92d64a
BlueThunderD <danag@DESKTOP-9KN3MO2>
2019-12-03 21:32:44 +0200

    Final code for lab5

--- src/main/java/db/Main.java
+++ src/main/java/db/Main.java
@@ -15,10 +15,10 @@ public class Main {
 	// update USER, PASS and DB URL according to credentials provided by the website:
 	// https://remotemysql.com/
 	// in future move these hard coded strings into separated config file or even better env variables
-	static private final String DB = "place_for_db_name";
+	static private final String DB = "ynF9e9kJi3";
 	static private final String DB_URL = "jdbc:mysql://remotemysql.com/"+ DB + "?useSSL=false";
-	static private final String USER = "place_for_username";
-	static private final String PASS = "place_for_password";
+	static private final String USER = "ynF9e9kJi3";
+	static private final String PASS = "i0InE30YTj";
 
 	public static void main(String[] args) throws SSLException {
 		Connection conn = null;
@@ -67,6 +67,89 @@ public class Main {
 				System.out.println("From: " + origin);
 			}
 			
+			
+			// Answer to a
+	
+			System.out.println("\t============");
+			sql = "update flights set price = ? where num = ?";
+			PreparedStatement prep_stmt2 = conn.prepareStatement(sql);
+			prep_stmt2.setInt(1, 2019);
+			prep_stmt2.setInt(2, 387);
+			prep_stmt2.executeUpdate();
+			
+			
+			
+			
+			
+			
+			// Answer to b
+			System.out.println("\t============");
+			System.out.println("Section b\n");
+			sql = "SELECT * FROM flights WHERE num= ?";
+			PreparedStatement prep_stmt3 = conn.prepareStatement(sql);
+			prep_stmt3.setInt(1, 387);
+			rs = prep_stmt3.executeQuery();
+			while (rs.next()) {
+				int num = rs.getInt("num");
+				String origin = rs.getString("origin");
+				String destination = rs.getString("destination");
+				int distance = rs.getInt("distance");
+				int price = rs.getInt("price");
+				System.out.format("Number %5s Origin %15s destinations %18s Distance %5d Price %5d\n", num, origin, destination, distance, price);
+			}
+			
+			
+			
+			// Answer to c.1
+			System.out.println("\t============");
+			System.out.println("Section c.1\n");
+			sql="SELECT * FROM flights WHERE distance > 1000";
+			Statement stmt1=conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+			ResultSet uprs=stmt1.executeQuery(sql);
+			while (uprs.next()) {
+				uprs.updateInt("price", uprs.getInt("price")+100);
+				uprs.updateRow();
+			}
+			printAll(conn);
+			
+			
+			// Answer to c.2
+			System.out.println("\t============");
+			System.out.println("Section c.2\n");
+			sql="SELECT * FROM flights WHERE price < 300";
+			uprs=stmt1.executeQuery(sql);
+			while (uprs.next()) {
+				uprs.updateInt("price", uprs.getInt("price")-25);
+				uprs.updateRow();
+			}
+			printAll(conn);
+			
+			
+			// Answer to d.1
+			System.out.println("\t============");
+			System.out.println("Section d.1\n");
+			sql = "update flights set price = price + ? where distance > ?";
+			PreparedStatement prep_stmt4 = conn.prepareStatement(sql);
+			prep_stmt4.setInt(1, 100);
+			prep_stmt4.setInt(2, 1000);
+			prep_stmt4.executeUpdate();
+			printAll(conn);
+			
+			
+			
+			// Answer to d.2
+			System.out.println("\t============");
+			System.out.println("Section d.2\n");
+			sql = "update flights set price = price - ? where price < ? AND price > 24";
+			PreparedStatement prep_stmt6 = conn.prepareStatement(sql);
+			prep_stmt6.setInt(1, 25);
+			prep_stmt6.setInt(2, 300);
+			prep_stmt6.executeUpdate();
+			printAll(conn);
+			
+			
+			
+			
 			rs.close();
 			stmt.close();
 			conn.close();
@@ -88,5 +171,25 @@ public class Main {
 				se.printStackTrace();
 			}
 		}
+	}
+	
+	public static void printAll(Connection conn ) {
+		String sql = "SELECT * FROM flights";
+		PreparedStatement prep_stmt5;
+		try {
+			prep_stmt5 = conn.prepareStatement(sql);
+			ResultSet rs = prep_stmt5.executeQuery();
+			while (rs.next()) {
+				int num = rs.getInt("num");
+				String origin = rs.getString("origin");
+				String destination = rs.getString("destination");
+				int distance = rs.getInt("distance");
+				int price = rs.getInt("price");
+				System.out.format("Number %5s Origin %15s destinations %18s Distance %5d Price %5d\n", num, origin, destination, distance, price);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
